ba66308d403787e6c55951a60f7549ac288ef129
Phuong-Dang <66028206+Phuong-Dang@users.noreply.github.com>
2020-06-09 10:01:46 -0700

    RockPaperScissors Assignment Hand-in

--- RockPaperScissors/src/Main.java
+++ RockPaperScissors/src/Main.java
@@ -4,67 +4,55 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        int r = 1;
-        int p = 2;
-        int s = 3;
+
         int user;
         int computer;
 
         System.out.println("Choose rock paper or scissors. '1' for rock, '2' for paper and '3' for scissors.");
-        int random = (int) (Math.random() * 5);
+        int random = (int) (Math.random() * 4);
         user = Integer.parseInt(scan.next());
 
 
         //User
         if (user>3)
             System.out.println("Invalid selection");
-        if (user == 1) {
-            System.out.println("Player chose: rock");
-        }
-        else if (user == 3){
-            System.out.println("Player chose: scissors");
-        }
-        else if (user == 2){
-            System.out.println("Player chose: paper");
-        }
         computer = random;
 
 
         //Computer
         if (user==1 && user==computer){
-            System.out.println("Computer chose: rock");
+            System.out.println("Player chose: rock   Computer chose: rock");
         }
         else if (user==1 && user>computer){
-            System.out.println("Computer chose: scissors");
+            System.out.println("Player chose: rock   Computer chose: scissors");
         }
         else if (user ==1 && user<computer){
-            System.out.println("Computer chose: paper");
+            System.out.println("Player chose: rock   Computer chose: paper");
         }
 
         if (user==2 && user==computer){
-            System.out.println("Computer chose: paper");
+            System.out.println("Player chose: paper  Computer chose: paper");
         }
         else if (user==2 && user>computer){
-            System.out.println("Computer chose: rock");
+            System.out.println("Player chose: paper  Computer chose: rock");
         }
         else if (user==2 && user<computer){
-            System.out.println("Computer chose: scissors");
+            System.out.println("Player chose: paper  Computer chose: scissors");
         }
 
         if (user==3 && user==computer){
-            System.out.println("Computer chose: scissors");
+            System.out.println("Player chose: scissors  Computer chose: scissors");
         }
         else if (user==3 && user>computer){
-            System.out.println("Computer chose: paper ");
+            System.out.println("Player chose: scissors  Computer chose: paper ");
         }
         else if (user==3 && user<computer){
-            System.out.println("Computer chose: rock");
+            System.out.println("Player chose: scissors  Computer chose: rock");
         }
 
 
         //Result
         if (user==computer){
-            System.out.println();
             System.out.println("Draw");
         }
         else if (user<computer){
